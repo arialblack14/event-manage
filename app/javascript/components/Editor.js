fc9ceaff91e4e1627429fcfb9d7f2805b0f663bb
Alex Tsiras <tsirasalexis@yahoo.gr>
2019-02-18 19:24:08 +0200

    Match params in Editor component

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import Header from './Header'
 import EventList from './EventList'
+import Event from './Event'
 
 class Editor extends React.Component {
   constructor(props) {
@@ -25,13 +26,26 @@ class Editor extends React.Component {
     const { events } = this.state
     if (events === null) return null;
 
+    const { match } = this.props
+    const eventId = match.params.id
+    const event = events.find(e => e.id === Number(eventId))
+
     return (
       <div>
         <Header />
         <EventList events={events} />
+        <PropsRoute path='/events/:id' component={Event} event={event} />
       </div>
     )
   }
+}
+
+Editor.propTypes = {
+  match: propTypes.shape(),
+}
+
+Editor.defaultProps = {
+  match: undefined,
 }
 
 export default Editor;
